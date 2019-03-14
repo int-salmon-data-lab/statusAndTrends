1980fbb409d9d9c1d4e44d446551c49f29136f74
Jim Tyhurst <jim@jimtyhurst.com>
2019-03-14 02:01:57 -0700

    Added functions to read biomass tables from Excel.

---
+++ R/read_biomass_tables.R
@@ -0,0 +1,119 @@
+library(readxl)
+
+ISDL_BIOMASS_DIR <- "North-Pacific-Salmon-Abundance-and-Biomass-Data"
+ISDL_BIOMASS_SPREADSHEET <- "mcf210023-sup-0001-tables1-s24.xlsx"
+
+#' Summary Table as a tibble from the biomass spreadsheet.
+#' @param sheet integer position of the sheet, where 1 is the first sheet.
+#' @param range string cell range. For example, "A6:P70". This range should include
+#'     the row of headings, as well as the data.
+read_biomass_summary_table <- function(sheet, range) {
+  system.file("extdata", ISDL_BIOMASS_DIR, ISDL_BIOMASS_SPREADSHEET, package = "statusAndTrends") %>% 
+    read_xlsx(sheet = sheet, range = range, na = "-")
+}
+
+#' Summary Table 1 'Natural-origin Pink Salmon' as a tibble.
+#' @description Returns a tibble containing data from Summary Table 1 of
+#'     the biomass data, which has abundance (catch and escapement in millions of fish) 
+#'     of natural-origin Pink Salmon returning to regions of Asia and 
+#'     North America, 1952-2015.
+#' @return tibble with 16 columns of data:
+#'    \enumerate{
+#'      \item Year
+#'      \item Korea
+#'      \item Japan
+#'      \item M&I					
+#'      \item WKam
+#'      \item EKam
+#'      \item WAK
+#'      \item SPen
+#'      \item Kod
+#'      \item CI
+#'      \item PWS
+#'      \item SEAK
+#'      \item NBC
+#'      \item SBC
+#'      \item WA
+#'      \item Total
+#'    }
+#' @export
+read_biomass_st1 <- function() {
+  read_biomass_summary_table(sheet = 5, range = "A6:P70")
+}
+
+#' Summary Table 2 'Natural-origin Chum Salmon' as a tibble.
+#' @description Returns a tibble containing data from Summary Table 2 of
+#'     the biomass data, which has abundance (catch and escapement in millions of fish) 
+#'     of natural-origin Chum Salmon returning to regions of Asia and 
+#'     North America, 1952-2015.
+#' @return tibble with 16 columns of data:
+#'    \enumerate{
+#'      \item Year
+#'      \item Korea
+#'      \item Japan
+#'      \item M&I					
+#'      \item WKam
+#'      \item EKam
+#'      \item WAK
+#'      \item SPen
+#'      \item Kod
+#'      \item CI
+#'      \item PWS
+#'      \item SEAK
+#'      \item NBC
+#'      \item SBC
+#'      \item WA
+#'      \item Total
+#'    }
+#' @export
+read_biomass_st2 <- function() {
+  read_biomass_summary_table(sheet = 5, range = "R6:AG70")
+}
+
+#' Summary Table 3 'Natural-origin Sockeye Salmon' as a tibble.
+#' @description Returns a tibble containing data from Summary Table 3 of
+#'     the biomass data, which has abundance (catch and escapement in millions of fish) 
+#'     of natural-origin Sockeye Salmon returning to regions of Asia and 
+#'     North America, 1952-2015.
+#'     Canadian spawning channel produced fish are included.
+#' @return tibble with 16 columns of data:
+#'    \enumerate{
+#'      \item Year
+#'      \item Korea
+#'      \item Japan
+#'      \item M&I					
+#'      \item WKam
+#'      \item EKam
+#'      \item WAK
+#'      \item SPen
+#'      \item Kod
+#'      \item CI
+#'      \item PWS
+#'      \item SEAK
+#'      \item NBC
+#'      \item SBC
+#'      \item WA
+#'      \item Total
+#'    }
+#' @export
+read_biomass_st3 <- function() {
+  read_biomass_summary_table(sheet = 5, range = "AI6:AX70")
+}
+
+#' Summary Table 4 'Total natural-origin salmon' as a tibble.
+#' @description Returns a tibble containing data from Summary Table 4 of
+#'     the biomass data, which has total natural-origin salmon (millions of fish) 
+#'     returning to regions of Asia and North America, 1952-2015.
+#' @return tibble with 5 columns of data:
+#'    \enumerate{
+#'      \item Pink
+#'      \item Chum
+#'      \item Sockeye
+#'      \item Total
+#'      \item Year				
+#'    }
+#' @export
+read_biomass_st4 <- function() {
+  read_biomass_summary_table(sheet = 5, range = "AZ6:BC70") %>% 
+    mutate(Year = 1952:2015)
+}
---
+++ tests/testthat/test_read_biomass_tables.R
@@ -0,0 +1,56 @@
+library(testthat)
+library(magrittr)
+library(dplyr)
+
+TOLERANCE = 0.01
+
+context("Reading biomass tables")
+
+test_that("Summary Table 1 has 64 rows", {
+  st1 <- read_biomass_st1()
+  expect_equal(nrow(st1), 64)
+})
+
+test_that("Summary Table 1 covers Year 1952 - 2015", {
+  st1 <- read_biomass_st1()
+  expect_equal(min(st1$Year), 1952)
+  expect_equal(max(st1$Year), 2015)
+})
+
+test_that("Summary Table 1 Total in 2015 is 442.8", {
+  actual_total <- read_biomass_st1() %>% 
+    dplyr::filter(Year == 2015) %>% 
+    magrittr::extract2("Total")
+  expect_equal(actual_total, 442.8, tolerance = TOLERANCE)
+})
+
+test_that("Summary Table 2 Total in 2015 is 86.9", {
+  actual_total <- read_biomass_st2() %>% 
+    dplyr::filter(Year == 2015) %>% 
+    magrittr::extract2("Total")
+  expect_equal(actual_total, 86.9, tolerance = TOLERANCE)
+})
+
+test_that("Summary Table 2 'Japan' column has no data", {
+  st2 <- read_biomass_st2()
+  expected_n_rows <- nrow(st2)
+  actual_n_na <- st2 %>% 
+    select(Japan) %>% 
+    is.na() %>% 
+    sum()
+  expect_equal(actual_n_na, expected_n_rows, tolerance = TOLERANCE)
+})
+
+test_that("Summary Table 3 Total in 2015 is 106.2", {
+  actual_total <- read_biomass_st3() %>% 
+    dplyr::filter(Year == 2015) %>% 
+    magrittr::extract2("Total")
+  expect_equal(actual_total, 106.2, tolerance = TOLERANCE)
+})
+
+test_that("Summary Table 4 Total in 2015 is 636.0", {
+  actual_total <- read_biomass_st4() %>% 
+    dplyr::filter(Year == 2015) %>% 
+    magrittr::extract2("Total")
+  expect_equal(actual_total, 636.0, tolerance = TOLERANCE)
+})
