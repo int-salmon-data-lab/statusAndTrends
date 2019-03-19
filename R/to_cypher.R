d47856ec5a1ce5aca864eaac62a1d9024181f38e
Jim Tyhurst <jim@jimtyhurst.com>
2019-03-19 02:44:50 -0700

    Generated script to create Datum.

--- R/to_cypher.R
+++ R/to_cypher.R
@@ -3,10 +3,26 @@ library(dplyr)
 library(uuid)
 
 #' @export
-build_datum_create_strings <- function(year, value, unit = 'count', place_label) {
+build_datum_create_strings <- function(
+  year, 
+  value, 
+  unit = 'count', 
+  place_label
+) {
   datum_uid <- uuid::UUIDgenerate()
-  create_statement <- sprintf("CREATE (n:Datum {value: '%s', unit: '%s', startInterval: '%s-01-01T00:00-UTC', endInterval: '%s-12-31T23:59-UTC', uid: '%s'});", value, unit, year, year, datum_uid)
-  match_statement <- sprintf("MATCH (x:Datum {uid: '%s'}), (y:Place {label: '%s'}) CREATE (x)-[r:hasPlace]->(y);", datum_uid, place_label)
+  create_statement <- sprintf(
+    "CREATE (n:Datum {value: '%s', unit: '%s', startInterval: '%s-01-01T00:00-UTC', endInterval: '%s-12-31T23:59-UTC', uid: '%s'});", 
+    value, 
+    unit, 
+    year, 
+    year, 
+    datum_uid
+  )
+  match_statement <- sprintf(
+    "MATCH (x:Datum {uid: '%s'}), (y:Place {label: '%s'}) CREATE (x)-[r:hasPlace]->(y);", 
+    datum_uid, 
+    place_label
+  )
   return(c(create_statement, match_statement))
 }
 
